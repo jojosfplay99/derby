efac90c4100c636883198b0d422362dc42fc6f39
Jobelle Ritche Albutra Cueme <55225816+jojosfplay99@users.noreply.github.com>
2023-12-19 04:40:53 +0800

    Update Server DB

--- modules/mainpage/server_db.php
+++ modules/mainpage/server_db.php
@@ -2,10 +2,11 @@
 
 // SQL server connection information
 $sql_details = array(
-    'user' => 'root',
-    'pass' => '',
-    'db'   => 'derby',
-    'host' => 'localhost'
+    'user' => 'if0_35636837',
+    'pass' => 'OyHT2aJMEcmLSrd',
+    'db'   => 'if0_35636837_derby',
+    'host' => 'sql112.infinityfree.com'
+    // ,'charset' => 'utf8' // Depending on your PHP and MySQL config, you may need this
 );
 
 ?>
